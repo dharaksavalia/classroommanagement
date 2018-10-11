e8980eb3606347c11c50a61482d003e623877558
dharaksavalia <dharak.savalia@gmail.com>
2018-10-11 11:45:30 -0400

    started with user admin

--- src/main/java/com/example/myapp/services/HelloService.java
+++ src/main/java/com/example/myapp/services/HelloService.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -31,6 +32,6 @@ public class HelloService {
 	(@PathVariable("helloId") int id) {
 			repository.deleteById(id);
 	}
-
+	
 }
 
