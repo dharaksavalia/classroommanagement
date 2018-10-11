8685238d74c610992b7b43e7b134fe5af18aa332
dharaksavalia <dharak.savalia@gmail.com>
2018-10-11 15:26:48 -0400

    fetching data in jquery\

--- src/main/webapp/jquery/components/user-admin/user-admin.controller.client.js
+++ src/main/webapp/jquery/components/user-admin/user-admin.controller.client.js
@@ -1,27 +1,31 @@
 //IIFE
 
 (function hello(){
+    var tbody;
+    var tr;
+    var trl;
 
     jQuery(main);
     function main(){
     var h1 = jQuery('h1');
-    h1.css('color','red');
-    h1.html('User Administration !');
-    var tr = $('.template');
-    var tr1 = tr.clone();
-    var tbody = $('tbody');
-        var users = [
-            {username: 'bob'},
-            {username: 'charlie'}
-        ];
+        tbody= $('tbody');
+        tr= $('.template');
+        var promise =fetch('http://localhost:8080/api/user');
+        promise.then((response)=>response.json()).then(renderUsers);
+
+    }
+    function renderUsers(users){
         for(var i=0;i<users.length;i++){
             var user=users[i];
-            console.log(user);
             tr1 = tr.clone();
             tr1.find('.username').html(user.username);
+            trl.find('.password').html(user.password);
+            // trl.find('.password').html(user.password);
+            // trl.find('.firstname').html(user.firstName);
+            // trl.find('.lastname').html(user.lastName);
+            console.log(user);
             tbody.append(tr1);
         }
     }
 
-    
 })();
