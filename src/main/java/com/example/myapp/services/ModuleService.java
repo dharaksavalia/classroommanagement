307daf3f29bf302218cee1a88c18f12369b73dae
dharaksavalia <dharaksavalia@github.com>
2018-10-29 11:16:03 -0400

    addition of post api/course/courseId/module

--- src/main/java/com/example/myapp/services/ModuleService.java
+++ src/main/java/com/example/myapp/services/ModuleService.java
@@ -1,12 +1,17 @@
 package com.example.myapp.services;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.myapp.models.Course;
 import com.example.myapp.models.Module;
 import com.example.myapp.repositories.CourseRepository;
 import com.example.myapp.repositories.ModuleRepository;
@@ -16,19 +21,20 @@ import com.example.myapp.repositories.ModuleRepository;
 public class ModuleService {
 	@Autowired
 	CourseRepository courseRepository;
-//	@Autowired
-////	ModuleRepository moduleRepository;
-////	public Module createModule(
-////			@PathVariable("courseId") int courseId,
-////			@RequestBody Module newModule) {
-////		Optional<Course> data = courseRepository.findById(courseId);
-////		if(data.isPresent()) {
-////			Course course = data.get();
-////			newModule.setCourse(course);
-////			return moduleRepository.save(newModule);
-////		}
-////		return null;
-//	}
+	@Autowired
+	ModuleRepository moduleRepository;
+	@PostMapping("/api/course/{courseId}/module")
+	public ResponseEntity<Module> createModule(
+			@PathVariable("courseId") int courseId,
+			@RequestBody Module newModule) {
+		Optional<Course> data = courseRepository.findById(courseId);
+		if(data.isPresent()) {
+			Course course = data.get();
+			newModule.setCourse(course);
+			return new ResponseEntity<>(moduleRepository.save(newModule),HttpStatus.OK);
+		}
+		return new ResponseEntity<>(newModule,HttpStatus.OK);
+	}
 
 
 }
