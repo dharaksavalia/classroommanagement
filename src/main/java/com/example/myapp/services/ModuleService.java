b807468edb408a604951e8c716f1b60ecba21d6b
dharaksavalia <dharaksavalia@github.com>
2018-10-29 13:21:08 -0400

    addition of delete api

--- src/main/java/com/example/myapp/services/ModuleService.java
+++ src/main/java/com/example/myapp/services/ModuleService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -46,6 +47,9 @@ public class ModuleService {
 		}
 		return new ResponseEntity<>(null,HttpStatus.BAD_GATEWAY);
 	}
-
-
+	@DeleteMapping("/api/module/{mId}")
+	public void deleteModule(@PathVariable("mId")int moduleId)
+	{
+		moduleRepository.deleteById(moduleId);
+	}
 }
